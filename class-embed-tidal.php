c5e2eab76e24af57cea5ce368af9f7facb21a5cf
Paul Schreiber <paul.schreiber@espn.com>
2018-03-12 13:32:42 -0400

    refactor in to class

--- class-embed-tidal.php
+++ class-embed-tidal.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Embed Tidal
- * Version: 0.1.1
+ * Version: 0.1.2
  * Description: Embed the Tidal web player via pasting a URL or using a shortcode. Works well with the Shortcake shortcode UI.
  * Author: Bjørn Johansen
  * Author URI: https://bjornjohansen.no
@@ -24,204 +24,239 @@ add_action( 'plugins_loaded', function () {
 	load_plugin_textdomain( 'embed-tidal', false, basename( dirname( __FILE__ ) ) . '/languages' );
 } );
 
-
-/**
- * Register the tidal shortcode
- *
- * @since 0.0.1
- */
-function embed_tidal_register_shortcode() {
-	add_shortcode( 'tidal', 'embed_tidal_shortcode' );
-}
-add_action( 'init', 'embed_tidal_register_shortcode' );
-
-
-/**
- * Shortcode UI setup for the tidal shortcode
- *
- * @since 0.0.1
- */
-function embed_tidal_shortcode_ui() {
-	/*
-	 * Define the UI for attributes of the shortcode.
-	 */
-	$fields = array(
-		array(
-			/* translators: Type of embed. I.e. album, playlist, track or video */
-			'label'   => esc_html_x( 'Type', 'noun', 'embed-tidal' ),
-			'attr'    => 'type',
-			'type'    => 'select',
-			'options' => array(
-				'a' => esc_html__( 'Album', 'embed-tidal' ),
-				'p' => esc_html__( 'Playlist', 'embed-tidal' ),
-				't' => esc_html_x( 'Track', 'embed type', 'embed-tidal' ),
-				'v' => esc_html__( 'Video', 'embed-tidal' ),
-			),
-		),
-		array(
-			'label' => esc_html__( 'Tidal ID', 'embed-tidal' ),
-			'attr'  => 'id',
-			'type'  => 'text',
-		),
-		array(
-			'label' => esc_html__( 'Tidal ID for related album', 'embed-tidal' ),
-			'attr'  => 'related-id',
-			'type'  => 'text',
-		),
-	);
+class Embed_Tidal {
 
 	/*
-	 * Define the Shortcode UI arguments.
+	 * Load the Tidal JavaScript only when a post needs it
 	 */
-	$shortcode_ui_args = array(
+	public static $load_script = false;
+
+	public static $embed_type = [];
+
+	public static function hooks() {
+		self::$embed_type = [
+			'a' => __( 'Album', 'embed-tidal' ),
+			'p' => __( 'Playlist', 'embed-tidal' ),
+			't' => _x( 'Track', 'embed type', 'embed-tidal' ),
+			'v' => __( 'Video', 'embed-tidal' ),
+		];
+
+		add_shortcode( 'tidal', [ __CLASS__, 'shortcode_handler' ] );
+		wp_embed_register_handler( 'tidal', '/^https?:\/\/(listen\.)?tidal.com\/(playlist|track|album|video)\/(.*)$/', [ __CLASS__, 'embed_handler' ] );
+
+		add_action( 'wp_footer', [ __CLASS__, 'enqueue_scripts' ] );
+		add_action( 'admin_enqueue_scripts', [ __CLASS__, 'admin_enqueue_scripts' ] );
+
+		add_action( 'plugins_loaded', function () {
+			load_plugin_textdomain( 'embed-tidal', false, basename( dirname( __FILE__ ) ) . '/languages' );
+		} );
 
 		/*
-		 * How the shortcode should be labeled in the UI.
+		 * Include a script for working with the shortcode UI
 		 */
-		'label' => esc_html__( 'Embed from Tidal', 'embed-tidal' ),
+		add_action( 'enqueue_shortcode_ui', function() {
+			wp_enqueue_script( 'tidal-embed-shortcode-ui', plugin_dir_url( __FILE__ ) . 'tidal-embed-shortcode-ui.js' );
+		} );
 
-		/*
-		 * Include an icon with your shortcode.
-		 * Use a dashicon, or full URL to image.
-		 */
-		'listItemImage' => 'dashicons-format-audio',
-
-		/*
-		 * Define the UI for attributes of the shortcode.
-		 *
-		 * See above, to where the the assignment to the $fields variable was made.
-		 */
-		'attrs' => $fields,
-	);
-
-	shortcode_ui_register_for_shortcode( 'tidal', $shortcode_ui_args );
-}
-add_action( 'register_shortcode_ui', 'embed_tidal_shortcode_ui' );
+		add_action( 'register_shortcode_ui', [ __CLASS__, 'embed_tidal_shortcode_ui' ] );
+	}
 
 
-/**
- * Callback for the tidal shortcode.
- *
- * @since 0.0.1
- *
- * @param array  $attr Provided attributes.
- * @param string $content The shortcode content.
- * @param string $shortcode_tag The shortcode tag.
- * @return string The rendered HTML.
- */
-function embed_tidal_shortcode( $attr, $content, $shortcode_tag ) {
-
-	$attr = shortcode_atts( array(
-		'type'       => 'a',
-		'id'         => null,
-		'related-id' => null,
-	), $attr, $shortcode_tag );
-
-	$html = '';
-
-	if ( strlen( $attr['type'] ) || strlen( $attr['id'] ) ) {
-
-		$related_attr = '';
-		if ( 'v' === $attr['type'] && strlen( $attr['related-id'] ) ) {
-			$related_attr = sprintf( ' data-related-id="%s"', esc_attr( $attr['related-id'] ) );
+	/**
+	 * Builds the content
+	 *
+	 * @since 0.1.2
+	 *
+	 * @param $atts
+	 *
+	 * @param array  $attr Provided attributes.
+	 * @param string $content The shortcode content.
+	 * @param string $shortcode_tag The shortcode tag.
+	 * @return string The rendered HTML.
+	 */
+	public static function get_content( $attr, $content = null, $shortcode_tag = null ) {
+		if ( is_admin() ) {
+			return 'Tidal ' . esc_html( self::$embed_type[ $attr['type'] ] ) . ' Embed (#' . esc_html( $attr['id'] ) . ')';
 		}
 
-		$html = sprintf( '<div class="tidal-embed" data-type="%s" data-id="%s"%s></div>', esc_attr( $attr['type'] ), esc_attr( $attr['id'] ), $related_attr );
-
-		if ( is_admin() || is_feed() ) {
-			$html .= '<script src="https://embed.tidal.com/tidal-embed.js"></script>';
-		} else {
-			wp_enqueue_script( 'embed-tidal', 'https://embed.tidal.com/tidal-embed.js', array(), null, true );
+		$result = sprintf( '<div class="tidal-embed" data-type="%s" data-id="%s"', esc_attr( $attr['type'] ), esc_attr( $attr['id'] ) );
+		if ( 'v' === $attr['type'] && isset( $attr['related_id'] ) && $attr['related_id'] ) {
+			$result .= sprintf( ' data-related-id="%s"', esc_attr( $attr['related_id'] ) );
 		}
+		$result .= '></div>';
+
+		return $result;
 	}
 
 	/**
-	 * Filters the tidal shortcode output.
+	 * Callback for the tidal shortcode.
 	 *
 	 * @since 0.0.1
 	 *
-	 * @param string $html Tidal shortcode HTML output.
-	 * @param array  $attr Array of tidal shortcode attributes.
+	 * @param array  $attr Provided attributes.
+	 * @param string $content The shortcode content.
+	 * @param string $shortcode_tag The shortcode tag.
+	 * @return string The rendered HTML.
 	 */
-	return apply_filters( 'tidal_shortcode', $html, $attr );
-}
+	public static function shortcode_handler( $attr, $content, $shortcode_tag ) {
+		self::$load_script = true;
 
+		$attr = shortcode_atts( [
+			'type'       => 'a',
+			'id'         => null,
+			'related_id' => null,
+		], $attr, $shortcode_tag );
 
-/*
- * Include a script for working with the shortcode UI
- */
-add_action( 'enqueue_shortcode_ui', function() {
-	wp_enqueue_script( 'tidal-embed-shortcode-ui', plugin_dir_url( __FILE__ ) . 'tidal-embed-shortcode-ui.js' );
-} );
-
-
-/**
- * The custom embed handler for Tidal URLs
- *
- * @since 0.0.2
- *
- * @param array  $matches The RegEx matches from the provided regex when calling wp_embed_register_handler().
- * @param array  $attr Embed attributes.
- * @param string $url The original URL that was matched by the regex.
- * @param array  $rawattr The original unmodified attributes.
- * @return string The embed HTML (or the original URL if we can’t handle it after all).
- */
-function embed_tidal_url_handler( $matches, $attr, $url, $rawattr ) {
-
-	$tidal_id = $matches[3];
-
-	switch ( $matches[2] ) {
-		case 'track':
-			$tidal_type = 't';
-			break;
-
-		case 'playlist':
-			$tidal_type = 'p';
-			break;
-
-		case 'video':
-			$tidal_type = 'v';
-			break;
-
-		case 'album':
-			$tidal_type = 'a';
-			break;
-
-		default:
-			return $url;
-			break;
+		/**
+		 * Filters the tidal shortcode output.
+		 *
+		 * @since 0.0.1
+		 *
+		 * @param string $html Tidal shortcode HTML output.
+		 * @param array  $attr Array of tidal shortcode attributes.
+		 */
+		return apply_filters( 'tidal_shortcode', self::get_content( $attr, $content, $shortcode_tag ), $attr );
 	}
 
-	$embed = sprintf( '<div class="tidal-embed" data-type="%s" data-id="%s"></div>', esc_attr( $tidal_type ), esc_attr( $tidal_id ) );
-
-	if ( is_admin() || is_feed() ) {
-		$embed .= '<script src="https://embed.tidal.com/tidal-embed.js"></script>';
-	} else {
-		wp_enqueue_script( 'embed-tidal', 'https://embed.tidal.com/tidal-embed.js', array(), null, true );
-	}
 
 	/**
-	 * Filters the Tidal URL embed output.
+	 * The custom embed handler for Tidal URLs
 	 *
 	 * @since 0.0.2
 	 *
-	 * @param string $embed   Embed HTML output.
 	 * @param array  $matches The RegEx matches from the provided regex when calling wp_embed_register_handler().
 	 * @param array  $attr Embed attributes.
 	 * @param string $url The original URL that was matched by the regex.
 	 * @param array  $rawattr The original unmodified attributes.
-	 * @return string The embed HTML.
+	 * @return string The embed HTML (or the original URL if we can’t handle it after all).
 	 */
-	return apply_filters( 'embed_tidal', $embed, $matches, $attr, $url, $rawattr );
+	public static function embed_handler( $matches, $attr, $url, $rawattr ) {
+		self::$load_script = true;
+
+		$attr = [ 'id' => $matches[3] ];
+
+		switch ( $matches[2] ) {
+			case 'track':
+				$attr['type'] = 't';
+				break;
+
+			case 'playlist':
+				$attr['type'] = 'p';
+				break;
+
+			case 'video':
+				$attr['type'] = 'v';
+				break;
+
+			case 'album':
+				$attr['type'] = 'a';
+				break;
+
+			default:
+				return $url;
+		}
+
+		/**
+		 * Filters the Tidal URL embed output.
+		 *
+		 * @since 0.0.2
+		 *
+		 * @param string $embed   Embed HTML output.
+		 * @param array  $matches The RegEx matches from the provided regex when calling wp_embed_register_handler().
+		 * @param array  $attr Embed attributes.
+		 * @param string $url The original URL that was matched by the regex.
+		 * @param array  $rawattr The original unmodified attributes.
+		 * @return string The embed HTML.
+		 */
+		return apply_filters( 'embed_tidal', self::get_content( $attr ), $matches, $attr, $url, $rawattr );
+	}
+
+	/**
+	 * Enqueue the Tidal JavaScript
+	 *
+	 * @since 0.1.2
+	 */
+	public static function enqueue_scripts() {
+		if ( ! self::$load_script ) {
+			return;
+		}
+
+		wp_enqueue_script( 'embed-tidal', 'https://embed.tidal.com/tidal-embed.js', [], null, true );
+	}
+
+	/**
+	 * Enqueue the Tidal JavaScript on admin pages
+	 *
+	 * @param string $hook
+	 */
+	public static function admin_enqueue_scripts( $hook ) {
+		if ( ! in_array( $hook, array( 'post.php', 'post-new.php' ), true ) ) {
+			return;
+		}
+
+		wp_enqueue_script( 'embed-tidal', 'https://embed.tidal.com/tidal-embed.js' );
+	}
+
+	/**
+	 * Shortcode UI setup for the tidal shortcode
+	 *
+	 * @since 0.0.1
+	 */
+	public static function embed_tidal_shortcode_ui() {
+		/*
+		 * Define the UI for attributes of the shortcode.
+		 */
+		$fields = array(
+			array(
+				/* translators: Type of embed. I.e. album, playlist, track or video */
+				'label'   => esc_html_x( 'Type', 'noun', 'embed-tidal' ),
+				'attr'    => 'type',
+				'type'    => 'select',
+				'options' => array(
+					'a' => esc_html__( 'Album', 'embed-tidal' ),
+					'p' => esc_html__( 'Playlist', 'embed-tidal' ),
+					't' => esc_html_x( 'Track', 'embed type', 'embed-tidal' ),
+					'v' => esc_html__( 'Video', 'embed-tidal' ),
+				),
+			),
+			array(
+				'label' => esc_html__( 'Tidal ID', 'embed-tidal' ),
+				'attr'  => 'id',
+				'type'  => 'text',
+			),
+			array(
+				'label' => esc_html__( 'Tidal ID for related album', 'embed-tidal' ),
+				'attr'  => 'related_id',
+				'type'  => 'text',
+			),
+		);
+
+		/*
+		 * Define the Shortcode UI arguments.
+		 */
+		$shortcode_ui_args = array(
+
+			/*
+			 * How the shortcode should be labeled in the UI.
+			 */
+			'label'         => esc_html__( 'Embed from Tidal', 'embed-tidal' ),
+
+			/*
+			 * Include an icon with your shortcode.
+			 * Use a dashicon, or full URL to image.
+			 */
+			'listItemImage' => 'dashicons-format-audio',
+
+			/*
+			 * Define the UI for attributes of the shortcode.
+			 *
+			 * See above, to where the the assignment to the $fields variable was made.
+			 */
+			'attrs'         => $fields,
+		);
+
+		shortcode_ui_register_for_shortcode( 'tidal', $shortcode_ui_args );
+	}
 }
 
-/**
- * Register the custom embed handler for Tidal URLs
- *
- * @since 0.0.2
- */
-function embed_tidal_wp_embed_register_handler() {
-	wp_embed_register_handler( 'tidal', '/^https?:\/\/(listen\.)?tidal.com\/(playlist|track|album|video)\/(.*)$/', 'embed_tidal_url_handler' );
-}
-add_action( 'init', 'embed_tidal_wp_embed_register_handler' );
-
+add_action( 'init', [ 'Embed_Tidal', 'hooks' ] );
